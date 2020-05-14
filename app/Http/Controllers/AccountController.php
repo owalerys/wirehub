9ae28eac63eb85840843fc3552a8b3ea04ca47fd
Oliver Walerys <oliver@walerys.com>
2020-05-13 23:39:16 -0400

    Team link endpoint

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -16,7 +16,7 @@ class AccountController extends Controller
 
     public function getAccount(string $accountId)
     {
-        $account = Account::where('external_id', $accountId)->with('item')->first();
+        $account = Account::where('external_id', $accountId)->with(['item', 'teams.users.roles'])->first();
 
         return response()->json($account);
     }
@@ -25,8 +25,23 @@ class AccountController extends Controller
     {
         $account = Account::where('external_id', $accountId)->first();
 
-        $paginated = $account->transactions()->orderBy('date', 'desc')->paginate();
+        $transactions = $account->transactions()->orderBy('date', 'desc')->limit(200)->get();
 
-        return response()->json($paginated);
+        return response()->json($transactions);
+    }
+
+    public function putTeamLink(string $accountId, Request $request)
+    {
+        $this->validate($request, [
+            'team_id' => 'required|exists:teams,id'
+        ]);
+
+        $account = Account::where('external_id', $accountId)->first();
+
+        if (!$account) return response('', 404);
+
+        $account->teams()->sync([$request->input('team_id')]);
+
+        return response()->json([]);
     }
 }
