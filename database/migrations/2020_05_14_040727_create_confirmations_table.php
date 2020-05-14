2047c51d5a8d3ee23900811d11ae53f160bc4402
Oliver Walerys <oliver@walerys.com>
2020-05-14 00:13:36 -0400

    Confirmations and plaid soft deletes migrations

---
+++ database/migrations/2020_05_14_040727_create_confirmations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfirmationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confirmations', function (Blueprint $table) {
+            $table->foreignId('transaction_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('confirmations');
+    }
+}
