92cb655f76a77854c7760973a4f970ebaf456c5e
Oliver Walerys <oliver@walerys.com>
2020-05-23 16:10:42 -0400

    Add ID field to transactions table

--- resources/js/components/tables/Transactions.vue
+++ resources/js/components/tables/Transactions.vue
@@ -11,48 +11,66 @@
                 {{ slotProps.value | money }}
             </template>
             <template v-slot:item.confirmed="slotProps">
-                {{ slotProps.item.confirmed ? 'Yes' : 'No' }}
+                {{ slotProps.item.confirmed ? "Yes" : "No" }}
             </template>
             <template v-slot:item.actions="slotProps">
-                <Confirmation :transaction="slotProps.item" @confirmed="updateConfirmedStatus(slotProps.item.external_id, $event)" />
+                <Confirmation
+                    :transaction="slotProps.item"
+                    @confirmed="
+                        updateConfirmedStatus(
+                            slotProps.item.external_id,
+                            $event
+                        )
+                    "
+                />
             </template>
         </v-data-table>
     </v-card>
 </template>
 
 <script>
-import Confirmation from '../Confirmation'
+import Confirmation from "../Confirmation";
 
 export default {
     components: {
         Confirmation
     },
-    props: ['loading', 'transactions', 'actions'],
+    props: ["loading", "transactions", "actions"],
     computed: {
         headers() {
             const headers = [
+                {
+                    text: "ID",
+                    align: "start",
+                    value: "id"
+                },
                 {
                     text: "Date",
                     align: "start",
                     value: "date"
                 },
                 { text: "Name", value: "name" },
-                { text: "Amount", value: "amount" },
-                { text: "Currency", value: "iso_currency_code" },
-                { text: "Confirmed", value: "confirmed" },
-            ]
+                { text: "Amount", value: "amount", align: "end" },
+                { text: "Currency", value: "iso_currency_code", align: "end" },
+                { text: "Confirmed", value: "confirmed", align: "end" }
+            ];
 
             if (this.actions) {
-                headers.push({ text: "Actions", value: "actions", sortable: false })
+                headers.push({
+                    text: "Actions",
+                    value: "actions",
+                    sortable: false,
+                    align: "end"
+                });
             }
 
-            return headers
+            return headers;
         }
     },
     methods: {
         updateConfirmedStatus(externalId, value) {
-            this.$emit('confirmed', { externalId, confirmed: value })
+            this.$emit("confirmed", { externalId, confirmed: value });
         }
     }
-}
+};
 </script>
