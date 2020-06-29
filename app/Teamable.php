e25877e0f009e4cfede25a1206c8557467c132ae
Oliver Walerys <oliver@walerys.com>
2020-06-29 19:20:41 -0400

    Convert teamable to model

--- app/Teamable.php
+++ app/Teamable.php
@@ -2,10 +2,11 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Teamable extends MorphPivot
+class Teamable extends Model
 {
     use SoftDeletes;
 }
