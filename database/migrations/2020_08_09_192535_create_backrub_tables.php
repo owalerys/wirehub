e7025c4fc57873f1c4cbbc9b41f7e5bcff581e49
Oliver Walerys <oliver@walerys.com>
2020-08-09 16:09:04 -0400

    Backrub migration

---
+++ database/migrations/2020_08_09_192535_create_backrub_tables.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBackrubTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('backrub_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('external_id');
+            $table->string('email');
+            $table->string('password');
+            $table->string('name')->nullable();
+            $table->string('error')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('backrub_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('external_id');
+            $table->string('name');
+            $table->string('nickname')->nullable();
+            $table->string('institution');
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('backrub_items');
+            $table->timestamps();
+        });
+
+        Schema::create('backrub_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('external_id');
+            $table->unsignedBigInteger('account_id');
+            $table->foreign('account_id')->references('id')->on('backrub_accounts');
+            $table->dateTime('posted_at');
+            $table->decimal('amount', 15, 2);
+            $table->string('currency');
+            $table->string('receiver_reference_number');
+            $table->string('sender_name');
+            $table->string('sender_address');
+            $table->string('sender_reference_number');
+            $table->string('receiver_name');
+            $table->string('receiver_bank_account_number');
+            $table->boolean('confirmed')->default(false);
+            $table->timestamp('confirmed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('backrub_items');
+        Schema::dropIfExists('backrub_accounts');
+        Schema::dropIfExists('backrub_transactions');
+    }
+}
