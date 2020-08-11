e81e23318b6e7b414bee62ba2f5c18af083419b3
Oliver Walerys <oliver@walerys.com>
2020-08-10 21:44:40 -0400

    Backrub create command

---
+++ app/Console/Commands/CreateBackrubItem.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\Backrub;
+use Illuminate\Console\Command;
+
+class CreateBackrubItem extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backrub:create-item {username} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new backrub item';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(Backrub $service)
+    {
+        $item = $service->connectItem($this->argument('username'), $this->argument('password'));
+
+        var_dump($item);
+    }
+}
