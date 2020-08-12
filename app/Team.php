da7ffb6c3cf02fc8d8087bc71c8553bd76480f1a
Oliver Walerys <oliver@walerys.com>
2020-08-12 18:54:05 -0400

    Add backrub accounts relation

--- app/Team.php
+++ app/Team.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Plaid\Account as PlaidAccount;
 use App\Flinks\Account as FlinksAccount;
+use App\Backrub\Account as BackrubAccount;
 use App\Services\Discovery;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
@@ -22,15 +23,22 @@ class Team extends Model
         return $this->morphedByMany(FlinksAccount::class, 'teamable')->using(Teamable::class);
     }
 
+    public function backrubAccounts()
+    {
+        return $this->morphedByMany(BackrubAccount::class, 'teamable')->using(Teamable::class);
+    }
+
     public function getAllAccounts(): Collection
     {
         $collection = collect();
 
         $this->load('plaidAccounts.item');
         $this->load('flinksAccounts.item');
+        $this->load('backrubAccounts.item');
 
         $collection = $collection->merge(Discovery::keyUniversal($this->plaidAccounts));
         $collection = $collection->merge(Discovery::keyUniversal($this->flinksAccounts));
+        $collection = $collection->merge(Discovery::keyUniversal($this->backrubAccounts));
 
         $this->allAccounts = $collection;
 
