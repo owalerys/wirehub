d6e03b7efb02fc56fa221939836d9ca8ccee8a29
Oliver Walerys <oliver@walerys.com>
2020-08-12 20:00:37 -0400

    Add wire meta to email template

--- app/Notifications/NewTransaction.php
+++ app/Notifications/NewTransaction.php
@@ -3,9 +3,8 @@
 namespace App\Notifications;
 
 use App\Contracts\Transaction as ContractsTransaction;
-use App\Transaction;
+use App\Contracts\Account as ContractsAccount;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -51,19 +50,49 @@ class NewTransaction extends Notification
         $description = $this->transaction->getName();
         $date = $this->transaction->getDate();
 
+        /** @var ContractsAccount $account */
+        $account = $this->transaction->account;
+
+        $accountName = $account->getNickname() ?: $account->getName();
+        $hasWireMeta = $account->hasWireMeta();
+        $hasAccountMeta = $account->hasAccountMeta();
+        $wireMeta = $this->transaction->getWireMeta();
+
         $parentResourceIdentifier = $this->transaction->getParentResourceIdentifier();
 
         $message = new MailMessage;
 
         $message->subject('A new transaction arrived.');
 
-        $message->line("Account: ***$mask");
+        if ($hasWireMeta) {
+            $message->line("Account: $accountName");
+        }
+
+        if ($hasAccountMeta) {
+            $message->line("Account: ***$mask");
+        }
+
         $message->line("Currency: $currency");
         $message->line("Amount: $amount");
 
         if ($pending !== null) $message->line("Pending: " . $pending ? 'YES' : 'NO');
 
-        $message->line("Description: $description");
+        if ($hasWireMeta) {
+            $receiverReference = $wireMeta->receiver->reference_number;
+            $senderReference = $wireMeta->sender->reference_number;
+            $senderName = $wireMeta->sender->name;
+            $senderAddress = $wireMeta->sender->address;
+
+            $message->line("Receiver Reference: $receiverReference");
+            $message->line("Sender Reference: $senderReference");
+            $message->line("Sender Name: $senderName");
+            $message->line("Sender Address: $senderAddress");
+        }
+
+        if ($hasAccountMeta) {
+            $message->line("Description: $description");
+        }
+
         $message->line("Date: $date");
 
         $message->action('View in Account', url(config('app.url') . "/app/accounts/$parentResourceIdentifier"));
