0b506189277f067370ff5ad1703c930648d94792
Oliver Walerys <oliver@walerys.com>
2021-02-11 16:53:01 -0500

    Offset scraping time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('items:force')->hourly();
+        $schedule->command('items:force')->hourlyAt(33);
     }
 
     /**
