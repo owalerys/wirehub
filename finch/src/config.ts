ec364a0b9287ae230ddf624e923ac96157f86a0f
Oliver Walerys <oliver@walerys.com>
2021-03-21 00:57:03 -0400

    Increase scrape connection timeout

--- finch/src/config.ts
+++ finch/src/config.ts
@@ -3,7 +3,7 @@ const {env} = process
 export const port = getNumber(env.PORT, 3000)
 export const maxPoolSize = getNumber(env.MAX_POOL_SIZE, 10)
 export const minPoolSize = getNumber(env.MIN_POOL_SIZE, 1)
-export const connectionTimeout = getNumber(env.CONNECTION_TIMEOUT, 150000)
+export const connectionTimeout = getNumber(env.CONNECTION_TIMEOUT, 60000 * 4)
 export const healthCheckEndpoint = env.HEALTH_CHECK_ENDPOINT
 export const headless = env.HEADLESS !== 'false'
 export const proxyHost = env.PROXY_HOST
