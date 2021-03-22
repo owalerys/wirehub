d885866c72d781c6e1a76cb6ca23dfa1ff2c9129
Oliver Walerys <oliver@walerys.com>
2021-03-22 17:03:41 -0400

    Make these logs a bit less noisy

--- finch/src/utils/debug.ts
+++ finch/src/utils/debug.ts
@@ -1,6 +1,6 @@
 import * as _debug from 'debug'
 
-_debug.enable("*");
+_debug.enable("puppeteer-as-a-service:*");
 
 export default function Debug (namespace) {
   return _debug(`puppeteer-as-a-service:${namespace}`)
