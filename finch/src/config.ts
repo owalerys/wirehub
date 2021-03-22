a95906c29b6ba84e72f7ea634ce474ad90d62a70
Oliver Walerys <oliver@walerys.com>
2021-03-22 16:07:23 -0400

    Reduce browsers to 3

--- finch/src/config.ts
+++ finch/src/config.ts
@@ -1,7 +1,7 @@
 const {env} = process
 
 export const port = getNumber(env.PORT, 3000)
-export const maxPoolSize = getNumber(env.MAX_POOL_SIZE, 10)
+export const maxPoolSize = getNumber(env.MAX_POOL_SIZE, 3)
 export const minPoolSize = getNumber(env.MIN_POOL_SIZE, 1)
 export const connectionTimeout = getNumber(env.CONNECTION_TIMEOUT, 60000 * 4)
 export const healthCheckEndpoint = env.HEALTH_CHECK_ENDPOINT
