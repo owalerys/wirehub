2ad2eddeda7f5d14899cd6c68b358be7effcf15c
Oliver Walerys <oliver@walerys.com>
2021-03-22 17:00:06 -0400

    These logs were shit

--- finch/src/server.ts
+++ finch/src/server.ts
@@ -10,6 +10,9 @@ import {
     healthCheckEndpoint,
     connectionTimeout
 } from "./config";
+import Debug from "./utils/debug";
+
+const debug = Debug("server");
 
 export default class FinchServer {
     private app = express();
@@ -28,11 +31,16 @@ export default class FinchServer {
                 res.end("Hey! I am Finch and I am healthy!");
             });
         }
+
+        debug("Creating server");
+
         this.server = createServer(async (req, res) => this.app(req, res)).on(
             "upgrade",
             (req: IncomingMessage, socket: Socket, head: any) => {
+                debug("Handling upgrade request %o", req);
                 this.upgrade(req, socket, head).catch(err => {
                     if (socket.writable) {
+                        debug("Upgrade failed %o", err);
                         console.error(err);
                         socket.end("HTTP/1.1 500 Internal Server Error");
                     }
@@ -41,6 +49,7 @@ export default class FinchServer {
         );
 
         this.proxy.on("error", (err, _req: IncomingMessage, res) => {
+            debug("Proxy error %o", err);
             if (res.writeHead) {
                 res.writeHead(500, { "Content-Type": "text/plain" });
             }
@@ -49,6 +58,7 @@ export default class FinchServer {
         });
 
         this.proxy.on("close", (_, socket) => {
+            debug("Proxy close %o", _)
             if (socket.writable) {
                 socket.end();
             }
@@ -66,11 +76,13 @@ export default class FinchServer {
     ): Promise<void> {
         let closed = false;
         const earlyClose = () => {
+            debug("Early close");
             closed = true;
         };
         socket.once("close", earlyClose);
 
         const browser = await this.browserPool.acquire().catch(err => {
+            debug("Browser not acquired %o", err);
             socket.end("HTTP/1.1 503 Service Unavailable");
             throw err;
         });
@@ -88,11 +100,13 @@ export default class FinchServer {
             const { port, pathname } = parse(browser.wsEndpoint());
             const target = `ws://127.0.0.1:${port}`;
             req.url = pathname as string | undefined;
+            debug("Init internal browser proxy %s/%s", target, req.url);
             this.proxy.ws(req, socket, head, { target });
         });
 
         const timeout = new Promise((_, reject) => {
             setTimeout(() => {
+                debug("Session timed out %o", socket);
                 if (socket.writable) {
                     socket.end("HTTP/1.1 408 Request Timeout");
                 }
@@ -103,6 +117,7 @@ export default class FinchServer {
         return Promise.race([handler, timeout]).then(
             () => this.browserPool.destroy(browser),
             async err => {
+                debug("Session failed %o", err);
                 await this.browserPool.destroy(browser);
                 throw err;
             }
--- finch/src/utils/debug.ts
+++ finch/src/utils/debug.ts
@@ -1,5 +1,7 @@
 import * as _debug from 'debug'
 
+_debug.enable("*");
+
 export default function Debug (namespace) {
   return _debug(`puppeteer-as-a-service:${namespace}`)
 }
